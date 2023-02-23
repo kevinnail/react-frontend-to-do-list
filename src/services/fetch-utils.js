6e39af62a9c99d165ac8d50db4ab69e7fb29d893
kevinnail <fumalicious@gmail.com>
2023-02-23 14:58:36 -0800

    added fetch-utils.js, contains fetch calls

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,134 @@
+// const BASE_URL = 'https://to-do-list-kn.herokuapp.com';
+const BASE_URL = 'http://localhost:7890';
+/* Auth related functions */
+
+export async function getUser() {
+  const resp = await fetch(`${BASE_URL}/api/v1/users/me`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  if (resp.ok) {
+    const user = await resp.json();
+    return user;
+  }
+}
+
+export async function signUpUser(email, password) {
+  const resp = await fetch(`${BASE_URL}/api/v1/users`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+    credentials: 'include',
+  });
+  const data = await resp.json();
+  if (resp.ok) {
+    location.replace('./');
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(data.message);
+  }
+}
+
+export async function signInUser(email, password) {
+  const resp = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+    credentials: 'include',
+  });
+
+  const data = await resp.json();
+
+  if (resp.ok) {
+    location.replace('/');
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(data.message);
+  }
+}
+
+export async function signOutUser() {
+  const resp = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
+    method: 'DELETE',
+    credentials: 'include',
+  });
+
+  if (resp.ok) {
+    location.replace('/auth');
+  }
+}
+
+/* Data functions */
+
+export async function fetchTodos() {
+  const resp = await fetch(`${BASE_URL}/api/v1/todos`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+  const data = await resp.json();
+
+  if (resp.ok) {
+    return data;
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(data.message);
+  }
+}
+
+export async function postToDo(task, user_Id) {
+  const resp = await fetch(`${BASE_URL}/api/v1/todos`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ task, user_Id }),
+    credentials: 'include',
+  });
+  const msg = await resp.json();
+  return msg;
+}
+
+export async function toggleComplete(mark, todo_id) {
+  const resp = await fetch(`${BASE_URL}/api/v1/todos/${todo_id}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ mark, todo_id }),
+    credentials: 'include',
+  });
+
+  const msg = await resp.json();
+  return msg;
+}
+export async function deleteById(todo_id) {
+  const resp = await fetch(`${BASE_URL}/api/v1/todos/${todo_id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    // body: JSON.stringify({ todo_id }),
+    credentials: 'include',
+  });
+  const msg = await resp.json();
+
+  return msg;
+}
