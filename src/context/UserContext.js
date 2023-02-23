3de8182c85667d15143632f4f8fdce85bb6c6daf
kevinnail <fumalicious@gmail.com>
2023-02-23 14:59:39 -0800

    updated import to fetch-utils.js for getUser

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react';
-import { getUser } from '../services/auth.js';
+import { getUser } from '../services/fetch-utils.js';
 
 const UserContext = createContext();
 
