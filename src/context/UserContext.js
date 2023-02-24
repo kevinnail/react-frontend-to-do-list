eba347755e328d6e53504134543db18e1454f642
kevinnail <fumalicious@gmail.com>
2023-02-23 22:28:29 -0800

    added useEffect to fetch User

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,13 +1,39 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { authUser } from '../services/auth.js';
 import { getUser } from '../services/fetch-utils.js';
 
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const currentUser = getUser();
-  const [user, setUser] = useState(currentUser);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
 
-  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await getUser();
+        setUser(user);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  const logInUser = (user) => {
+    setUser(user);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ user, setUser, error, setError, loading, setLoading, logInUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 const useUser = () => {
