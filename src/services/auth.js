050076961bb126279d095b8473085f2f6a472ba9
kevinnail <fumalicious@gmail.com>
2023-02-24 15:21:36 -0800

    removed comments

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,18 +1,11 @@
 import { signInUser, signOutUser, signUpUser } from './fetch-utils.js';
 
-// import { client } from './client';
-
-// export function getUser() {
-//   return client.auth.currentUser;
-// }
-
 export async function authUser(email, password, type) {
   let response;
   if (type === 'sign-up') {
-    // response = await client.auth.signUp({ email, password });
-    response = await signUpUser({ email, password });
+    response = await signUpUser(email, password);
   } else {
-    response = await signInUser({ email, password });
+    response = await signInUser(email, password);
   }
   if (response.error) {
     throw response.error;
@@ -21,6 +14,5 @@ export async function authUser(email, password, type) {
 }
 
 export async function signOut() {
-  // await client.auth.signOut();
   await signOutUser();
 }
