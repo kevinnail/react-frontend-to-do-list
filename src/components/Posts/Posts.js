2ede84a1ddcf22cc7f84f3138d94c824101ae1b6
kevinnail <fumalicious@gmail.com>
2023-02-24 15:19:54 -0800

    added UserContext to export, removed useUser

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { useUser } from '../../context/UserContext.js';
+import { useUser } from '../../hooks/useUser.js';
 
 export default function Posts() {
   const { user } = useUser();
+  // const history = useHistory();
+  console.log('user in Posts.js', user);
+  // console.log('history in Posts.js', history.replace);
+  console.log('page reloads on Posts.js');
 
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
