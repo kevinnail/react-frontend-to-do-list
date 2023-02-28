c14e37c89f3ad014ed91443fa6a8a332d048abb9
kevinnail <fumalicious@gmail.com>
2023-02-28 14:49:52 -0800

    added NewPost files, and route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 // import { useUser } from './context/UserContext.js';
 import Header from './components/Header/Header.js';
 import Posts from './components/Posts/Posts.js';
+import NewPost from './components/NewPost/NewPost.js';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
+        <Route path="/todos/new" component={NewPost} />
         <Route path="/todos" component={Posts} />
         <Route exact path="*">
           <Redirect to="/auth/sign-in" />
