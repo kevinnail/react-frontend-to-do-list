651f817bc1ccc657e31b219708f21c224152eff9
kevinnail <fumalicious@gmail.com>
2023-02-28 11:23:39 -0800

    code clean up

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,11 +4,6 @@ import { useUser } from '../../hooks/useUser.js';
 
 export default function Posts() {
   const { user } = useUser();
-  // const history = useHistory();
-  console.log('user in Posts.js', user);
-  // console.log('history in Posts.js', history.replace);
-  console.log('page reloads on Posts.js');
-
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './context/UserContext.js';
 import { BrowserRouter } from 'react-router-dom';
+// import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
