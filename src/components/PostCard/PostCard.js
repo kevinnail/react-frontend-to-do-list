e9da3b32f7adcbaa9699b9572874b2e798d98465
kevinnail <fumalicious@gmail.com>
2023-03-01 12:56:02 -0800

    added some css to buttons, added handleClick

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { usePosts } from '../../hooks/usePosts.js';
 import { useUser } from '../../hooks/useUser.js';
@@ -6,6 +7,8 @@ import './PostCard.css';
 export default function PostCard({ task, id }) {
   const { user } = useUser();
   const { error } = usePosts();
+  const [finished, setFinished] = useState(false);
+
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
@@ -13,17 +16,32 @@ export default function PostCard({ task, id }) {
   if (error) {
     return <div>Error: {error}</div>;
   }
+  const handleClick = (e) => {
+    if (e.target.name === 'edit') {
+      console.log('edit');
+    } else if (e.target.name === 'delete') {
+      console.log('delete');
+    } else {
+      console.log('finished before setter', finished);
+      setFinished((prevFinished) => {
+        console.log('finished after setter', !prevFinished);
+        return !prevFinished;
+      });
+    }
+  };
 
   return (
     <div className="post overlay" key={id}>
       <div>
-        <img src="/edit.png" alt="edit" />
+        <img className="buttons" onClick={handleClick} name="edit" src="/edit.png" alt="edit" />
       </div>
       <div>
         {' '}
-        <img src="/delete.png" alt="edit" />
+        <img className="buttons" onClick={handleClick} src="/delete.png" name="delete" alt="edit" />
       </div>
-      <h1>{task}</h1>
+      <h1 className="todo" onClick={handleClick} name="finished">
+        {task}
+      </h1>
     </div>
   );
 }
