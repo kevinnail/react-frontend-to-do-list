deb362814ff129459f75e144ff4f09a814532fdc
kevinnail <fumalicious@gmail.com>
2023-02-28 22:07:11 -0800

    add loading state, simple display while list loads

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,12 +6,20 @@ import PostCard from '../PostCard/PostCard.js';
 
 export default function Posts() {
   const { user } = useUser();
-  const { posts } = usePosts();
+  const { posts, loading } = usePosts();
 
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
+  console.log('loading: ', loading);
 
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
   return (
     <div className="list-container">
       {posts.map((post) => (
