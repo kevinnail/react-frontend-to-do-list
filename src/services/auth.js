1b7f10b49086ca23a0b35495cc122a04620bc0ef
kevinnail <fumalicious@gmail.com>
2023-03-04 12:27:43 -0800

    added signIn to signUp, adjusted url

--- src/services/auth.js
+++ src/services/auth.js
@@ -4,8 +4,10 @@ export async function authUser(email, password, type) {
   let response;
   if (type === 'sign-up') {
     response = await signUpUser(email, password);
+    // console.log('response-sign up', response);
   } else {
     response = await signInUser(email, password);
+    // console.log('response-sign in', response);
   }
   if (response.error) {
     throw response.error;
